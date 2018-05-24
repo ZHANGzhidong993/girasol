d294d3c51347368046d9385e2a0f78f41da65fcf
张志栋 <zhangzhidong@zhangzhidongdeMacBook-Pro.local>
2018-05-25 00:23:12 +0800

    Initial commit

---
+++ registry-center/src/main/java/com/myhexin/girasol/registry/center/RegistryCenterApplication.java
@@ -0,0 +1,12 @@
+package com.myhexin.girasol.registry.center;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryCenterApplication {
+    public static void main(String[] args) {
+
+    }
+}
